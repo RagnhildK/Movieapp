159a2b3636076d2c5dc7219e4cdfbaa5a087b24d
Ragnhild <ragnhildk98@gmail.com>
2022-01-27 11:09:14 +0100

    display movie poster

--- components/Movie/Movie.js
+++ components/Movie/Movie.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import RatingButtons from "./RatingButtons";
 
 
-function Movie({ title, img, overview }) {
+function Movie({ title, img, overview, posterPath }) {
   // Renders the movie that is passed as function input
+  let url = 'https://image.tmdb.org/t/p/w500/' + posterPath
   return (
     <View className="MovieItem">
       <Text>{title}</Text>
-      {/* <Image source={img} /> */}
+      <Image style={{width: 200, height: 300}} source={{uri: url}}></Image>
       <Text>{overview}</Text>
       <RatingButtons/>
     </View>
