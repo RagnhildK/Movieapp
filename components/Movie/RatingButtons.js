d9396f4bde7d13ae2fd97e15a1679934666ff963
Ragnhild <ragnhildk98@gmail.com>
2022-01-27 16:32:02 +0100

    add react-native-rating

--- components/Movie/RatingButtons.js
+++ components/Movie/RatingButtons.js
@@ -2,24 +2,19 @@ import React from "react";
 import { View, Button } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { rateMovie } from "../../redux/movieSlicer";
+import { Rating } from "react-native-ratings";
 
 function RatingButtons({ id }) {
-  // Renders the movie that is passed as function input
   const { movies } = useSelector((state) => state.movieRatings);
   const dispatch = useDispatch();
 
   const handlePress = (rating) => {
-    dispatch(rateMovie({rating: rating, id: id}));
+    dispatch(rateMovie({ rating: rating, id: id }));
   };
 
   return (
     <View className="MovieItem">
-      <Button onPress={() => handlePress(0)} title="0" />
-      <Button onPress={() => handlePress(1)} title="1" />
-      <Button onPress={() => handlePress(2)} title="2" />
-      <Button onPress={() => handlePress(3)} title="3" />
-      <Button onPress={() => handlePress(4)} title="4" />
-      <Button onPress={() => handlePress(5)} title="5" />
+      <Rating></Rating>
     </View>
   );
 }
