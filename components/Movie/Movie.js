0708a11756364e3d40e6d0cf38a9cfd23d8e5f11
Ragnhild <ragnhildk98@gmail.com>
2022-01-27 11:37:54 +0100

    part of merge

--- components/Movie/Movie.js
+++ components/Movie/Movie.js
@@ -3,21 +3,13 @@ import { View, Text, Image } from "react-native";
 import RatingButtons from "./RatingButtons";
 
 
-<<<<<<< HEAD
 function Movie({ title, img, overview, posterPath }) {
-=======
-function Movie({ title, posterPath, overview }) {
->>>>>>> origin/Skeleton
   // Renders the movie that is passed as function input
   let url = 'https://image.tmdb.org/t/p/w500/' + posterPath
   return (
     <View className="MovieItem">
       <Text>{title}</Text>
-<<<<<<< HEAD
       <Image style={{width: 200, height: 300}} source={{uri: url}}></Image>
-=======
-      <Image style={{width: '20%', height: '5%'}} source={{uri: 'https://media.snl.no/media/162191/article_topimage_shutterstock_1029842671.jpg'}}></Image>
->>>>>>> origin/Skeleton
       <Text>{overview}</Text>
       <RatingButtons/>
     </View>
