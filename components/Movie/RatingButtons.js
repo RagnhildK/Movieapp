9f1547953779e62ae1bb8f9bc577be94d94bba66
Hanna <hanntorj@stud.ntnu.no>
2022-01-27 11:28:51 +0100

    merge to dev

--- components/Movie/RatingButtons.js
+++ components/Movie/RatingButtons.js
@@ -1,21 +1,26 @@
 import React from "react";
 import { View, Button } from "react-native";
+import { useDispatch } from "react-redux";
+import { addRatedMovie } from "../../redux/movie";
 
 function RatingButtons({ title, img, abstract }) {
   // Renders the movie that is passed as function input
 
+  const dispatch = useDispatch();
+
   const handlePress = (rating) => {
-    
+    // const movie = 
+    dispatch(addRatedMovie());
   };
 
   return (
     <View className="MovieItem">
-      <Button onPress={()=>handlePress(0)} title="0" />
-      <Button onPress={()=>handlePress(1)} title="1" />
-      <Button onPress={()=>handlePress(2)} title="2" />
-      <Button onPress={()=>handlePress(3)} title="3" />
-      <Button onPress={()=>handlePress(4)} title="4" />
-      <Button onPress={()=>handlePress(5)} title="5" />
+      <Button onPress={() => handlePress(0)} title="0" />
+      <Button onPress={() => handlePress(1)} title="1" />
+      <Button onPress={() => handlePress(2)} title="2" />
+      <Button onPress={() => handlePress(3)} title="3" />
+      <Button onPress={() => handlePress(4)} title="4" />
+      <Button onPress={() => handlePress(5)} title="5" />
     </View>
   );
 }
