b9e1fa6a6998fcb45b7aefe53d9e771dad0d0884
Ragnhild <ragnhildk98@gmail.com>
2022-01-28 19:30:16 +0100

    register user

--- server.mjs
+++ server.mjs
@@ -10,7 +10,10 @@ await client.connect();
 console.log("connected");
 
 const sessionCollection = client.db("movieApp").collection("session");
+const userCollection = client.db("movieApp").collection("users");
+
 // client.close();
+
 `
 session : {
     ownerUsername: aka id string,
@@ -24,24 +27,42 @@ session : {
 
 Example:
 
-{
-    ownerUsername: ozan,
-    moviesTBRated: [1,2,3,4],
-    participants: {
-        ragnhild: {
-            1: 5,
-            2: 2,...
-        },
-        hannah: {
-            1: 1,
-            ...
-        },
-        ozan: null, // Ozan is lazy
-    }
-}
+[
+  {
+      ownerUsername: ozan,
+      moviesTBRated: [1,2,3,4],
+      participants: {
+          ragnhild: {
+              1: 5,
+              2: 2,...
+          },
+          hannah: {
+              1: 1,
+              ...
+          },
+          ozan: null, // Ozan is lazy
+      }
+  },
+]
+
+users : [{username: }, {username: }, ...]
+
+users: [{username: "Ozan"}, {username: "Ragnhild"}, {username: "Hanna"}]
 `;
 
-const createASession = async (ownerUsername, moviesTBRated) => {
+//TODO: store username in asyncstorage as well
+//
+
+export const registerUser = async (username) => {
+  if ((await userCollection.count({ username })) > 0) {
+    console.log(username + " username is used");
+  } else {
+    console.log(username + " username is avaliable");
+    await userCollection.insertOne({ username });
+  }
+};
+
+export const createASession = async (ownerUsername, moviesTBRated) => {
   // Insert a document with the owners username
   await deleteASession(ownerUsername);
   await sessionCollection.insertOne({
@@ -52,7 +73,7 @@ const createASession = async (ownerUsername, moviesTBRated) => {
   await joinASession(ownerUsername, ownerUsername);
 };
 
-const joinASession = async (myUsername, ofUsername) => {
+export const joinASession = async (myUsername, ofUsername) => {
   // Check the existance of the session
   // Add a username to the session document ofUsername
   const key = `partipicants.${myUsername}`;
@@ -66,7 +87,8 @@ const joinASession = async (myUsername, ofUsername) => {
   await sessionCollection.updateOne(filterDocument, updateDocument);
 };
 
-const updateRatings = async (myUsername, ofUsername, ratings) => {
+//should be called when a user submits their ratings
+export const updateRatings = async (myUsername, ofUsername, ratings) => {
   // Save the ratings of the user in the session of the
   const key = `partipicants.${myUsername}`;
   const set = {};
@@ -78,25 +100,36 @@ const updateRatings = async (myUsername, ofUsername, ratings) => {
   await sessionCollection.updateOne(filterDocument, updateDocument);
 };
 
-const getRatings = async (ofUsername) => {
+//should be called by another business logic. run periodically when owner is done rating
+export const getRatings = async (myUsername, ofUsername) => {
   //return an object with all participants and their ratings
-  const results = await (
-    await sessionCollection.find({ ownerUsername: ofUsername })
-  ).toArray();
-  console.log(results[0].partipicants);
-  return results[0].partipicants;
+  //check that the user that asks for ratings is the owner of the session
+  if (myUsername !== ofUsername) {
+    console.log("Error! You are not the owner of this session");
+    return null;
+  } else {
+    const result = await sessionCollection.findOne({
+      ownerUsername: ofUsername,
+    });
+    return result.partipicants;
+  }
 };
 
-const deleteASession = async (ownerUsername) => {
+//should be called when owener of session clickes "end session" on results screen
+export const deleteASession = async (ownerUsername) => {
   await sessionCollection.deleteMany({
     ownerUsername,
   });
 };
 
-await createASession("Ozan", [1, 2, 3]);
-await joinASession("Ragnhild", "Ozan");
-await updateRatings("Ragnhild", "Ozan", { 1: 5, 2: 2, 3: 1 });
-await createASession("Hanna", [3, 4, 5]);
-await updateRatings("Hanna", "Hanna", { 3: 2, 4: 5, 5: 4 });
-await getRatings("Ozan");
-await deleteASession("Hanna");
+await registerUser("Ozan");
+await registerUser("Raggen");
+await registerUser("Ozan");
+// await createASession("Ozan", [1, 2, 3]);
+// await joinASession("Ragnhild", "Ozan");
+// await updateRatings("Ragnhild", "Ozan", { 1: 5, 2: 2, 3: 1 });
+// await createASession("Hanna", [3, 4, 5]);
+// await updateRatings("Hanna", "Hanna", { 3: 2, 4: 5, 5: 4 });
+// await getRatings("Hanna", "Ozan");
+// await getRatings("Ozan", "Ozan");
+// await deleteASession("Hanna");
