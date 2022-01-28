04c5d16054165852835ab15c9bb78ec5d6f33c79
Hanna <hanntorj@stud.ntnu.no>
2022-01-28 19:31:32 +0100

    r

--- Screens/CreatorScreens/WaitingScreen.js
+++ Screens/CreatorScreens/WaitingScreen.js
@@ -5,8 +5,9 @@ export default function WaitingScreen({ navigation }) {
   return (
     <View>
       <Text>Wait for other members or end session.</Text>
+      <Text>Wait for other members or end session.</Text>
       <Button
-        title="This page should go to the resultsScreen"
+        title="End session and view results"
         onPress={() => navigation.navigate("ResultScreen")}
       />
     </View>
