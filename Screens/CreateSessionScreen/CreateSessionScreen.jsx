1346877c78f169a0af6d6d841775a6040d4408c2
Hanna <hanntorj@stud.ntnu.no>
2022-02-01 16:45:40 +0100

    padding create/join session

--- Screens/CreateSessionScreen/CreateSessionScreen.jsx
+++ Screens/CreateSessionScreen/CreateSessionScreen.jsx
@@ -1,24 +1,22 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import {
-  View, StyleSheet, Pressable, Text, SafeAreaView
-} from 'react-native';
-import { TextInput, Headline } from 'react-native-paper';
-import { setUsername, setLoading, setSessionID } from '../../redux/movieSlicer';
-import * as Colors from '../../styles/colors';
-import { addSession } from '../../utils/firebase';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { View, StyleSheet, Pressable, Text, SafeAreaView } from "react-native";
+import { TextInput, Headline } from "react-native-paper";
+import { setUsername, setLoading, setSessionID } from "../../redux/movieSlicer";
+import * as Colors from "../../styles/colors";
+import { addSession } from "../../utils/firebase";
 
 export default function CreateSessionScreen({ navigation }) {
-  const [localUsername, setLocalUsername] = useState('');
-  const [localSessionID, setLocalSessionID] = useState('');
+  const [localUsername, setLocalUsername] = useState("");
+  const [localSessionID, setLocalSessionID] = useState("");
   const [userError, setUserError] = useState(false);
   const [sessionError, setSessionError] = useState(false);
 
   const dispatch = useDispatch();
 
   const createSession = () => {
-    const validUsername = localUsername !== '';
-    const validSession = localSessionID !== '';
+    const validUsername = localUsername !== "";
+    const validSession = localSessionID !== "";
     if (!validUsername) {
       if (!validSession) {
         setUserError(true);
@@ -35,42 +33,44 @@ export default function CreateSessionScreen({ navigation }) {
       dispatch(setSessionID(localSessionID));
       dispatch(setLoading(true));
       addSession(localUsername, localSessionID);
-      navigation.navigate('RatingScreen');
+      navigation.navigate("RatingScreen");
     }
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Headline style={styles.heading1}>What's your nickname?</Headline>
-      <TextInput
-        mode="outlined"
-        label="Nickname"
-        error={userError}
-        onChangeText={(val) => setLocalUsername(val)}
-        value={localUsername}
-        placeholder="Enter your nickname..."
-        style={styles.input}
-      />
-      <Headline style={styles.heading2}>What's the name of the party?</Headline>
-      <TextInput
-        mode="outlined"
-        label="Name of movie party"
-        error={sessionError}
-        onChangeText={(val) => setLocalSessionID(val)}
-        value={localSessionID}
-        placeholder="Enter a name for the party..."
-      />
-      <Pressable style={styles.button} onPress={() => createSession()}>
-        <Text style={styles.buttonText}>Start a party</Text>
-      </Pressable>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <SafeAreaView>
+        <Headline style={styles.heading1}>What's your nickname?</Headline>
+        <TextInput
+          mode="outlined"
+          error={userError}
+          onChangeText={(val) => setLocalUsername(val)}
+          value={localUsername}
+          placeholder="Enter your nickname..."
+          style={styles.input}
+        />
+        <Headline style={styles.heading2}>
+          What's the name of the party?
+        </Headline>
+        <TextInput
+          mode="outlined"
+          error={sessionError}
+          onChangeText={(val) => setLocalSessionID(val)}
+          value={localSessionID}
+          placeholder="Enter a name for the party..."
+        />
+        <Pressable style={styles.button} onPress={() => createSession()}>
+          <Text style={styles.buttonText}>Start a party</Text>
+        </Pressable>
+      </SafeAreaView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     backgroundColor: Colors.DARK_PURPLE,
     paddingLeft: 20,
     paddingRight: 20,
@@ -82,26 +82,26 @@ const styles = StyleSheet.create({
     padding: 10,
     maxWidth: 150,
     borderRadius: 10,
-    alignSelf:"flex-end"
+    alignSelf: "flex-end",
   },
   buttonText: {
     color: Colors.WHITE,
-    textAlign: 'center',
+    textAlign: "center",
   },
   heading1: {
     color: Colors.ORANGE_LIGHT,
     fontSize: 24,
     margin: 30,
-    textAlign: 'center',
-    fontStyle: 'italic',
+    textAlign: "center",
+    fontStyle: "italic",
   },
   heading2: {
     color: Colors.ORANGE_LIGHT,
     fontSize: 24,
     margin: 30,
     marginTop: 40,
-    textAlign: 'center',
-    fontStyle: 'italic',
+    textAlign: "center",
+    fontStyle: "italic",
   },
   intput: {
     //funmker ikke
